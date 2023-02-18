cd5889f441b6faf86ffd483cf2cb4f9e38248ea0
Johnathan Walker <johnathan-walker@users.noreply.github.com>
2023-02-18 06:49:09 -0500

    I understand mutable arguments vs mutable borrows now.

--- sorts/src/merge.rs
+++ sorts/src/merge.rs
@@ -5,29 +5,29 @@ mod tests {
     fn run_tests() {
         let input = [3u32, 2, 1, 4, 26, 17];
         let output = [1u32, 2, 3, 4, 17, 26];
-        let result = super::sort(&mut input[..].to_owned());
+        let result = super::sort(input.to_vec());
         assert_eq!(result[..], output[..])
     }
 }
 
-pub fn sort<T: Ord + Clone + Debug + Copy>(arr: &mut Vec<T>) -> Vec<T> {
+pub fn sort<T: Ord + Clone + Debug + Copy>(arr: Vec<T>) -> Vec<T> {
     merge_sort(arr)
 }
 
-fn merge_sort<T: Ord + Clone + Debug + Copy>(arr: &Vec<T>) -> Vec<T> {
+fn merge_sort<T: Ord + Clone + Debug + Copy>(arr: Vec<T>) -> Vec<T> {
     if arr.len() < 2 {
         return arr.to_vec();
     }
 
     let mid = arr.len() / 2;
-    let mut l = merge_sort(&arr[..mid].to_vec());
-    let mut r = merge_sort(&arr[mid..].to_vec());
-    let result = merge(&mut l, &mut r);
+    let l = merge_sort(arr[..mid].to_vec());
+    let r = merge_sort(arr[mid..].to_vec());
+    let result = merge(l, r);
 
     return result;
 }
 
-fn merge<T: Ord + Debug + Copy>(l: &mut Vec<T>, r: &mut Vec<T>) -> Vec<T> {
+fn merge<T: Ord + Debug + Copy>(mut l: Vec<T>, mut r: Vec<T>) -> Vec<T> {
     let mut result = Vec::new();
     while l.len() > 0 && r.len() > 0 {
         if r.first().unwrap() > l.first().unwrap() {
