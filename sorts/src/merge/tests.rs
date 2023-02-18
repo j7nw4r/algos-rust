b19a5dac3c135b44532ffe68b488fc91317cf308
Johnathan Walker <johnathan-walker@users.noreply.github.com>
2023-02-18 06:15:53 -0500

    Removing un-needed files.

--- sorts/src/merge/tests.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn run_tests() {
-    let mut input = [3u32, 2, 1, 4, 26, 17];
-    let output = [1u32, 2, 3, 4, 17, 26];
-
-    super::sort(&mut input);
-
-    assert_eq!(input[..], output[..])
-}
